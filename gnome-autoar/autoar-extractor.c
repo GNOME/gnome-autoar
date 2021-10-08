GNOME/gnome-autoar

commit b9590ab77b70e74e9deffd2af6c32908dc3c5aaf

extractor: Prevent redundant path name handling for equal prefixes
Currently, redundant path name handling happens when archive files have
a common prefix and an equal destination is returned from the `decide-destination`
signal. In this case, the old prefix is removed and the new one (the equal one)
is added again. Let's detect this case and prevent the redundancy.

gnome-autoar/autoar-extractor.c
@@ -1817,9 +1817,12 @@ autoar_extractor_step_decide_destination (AutoarExtractor *self)
     autoar_extractor_signal_decide_destination (self,
                                                 self->prefix,
                                                 files,
-                                                &new_destination);
+                                                &self->new_prefix);
 
-    self->new_prefix = new_destination;
+    if (self->new_prefix && g_file_equal (self->prefix, self->new_prefix)) {
+      /* This prevents redundant path name handling later. */
+      g_clear_object (&self->new_prefix);
+    }
   } else {
     autoar_extractor_signal_decide_destination (self,
                                                 self->destination_dir,

commit 8626035793abe4bab4f30b2c4a306e28f34c4041

extractor: Fix extraction in symlinked locations
The "The file is not a directory" error is returned in case the archive has
common prefix, which have not been changed over the `decide-destination`
signal and the destination is a symlink. This is regression caused by the
commit b9590ab7. It causes that the security checks, which was added by the
commit 8109c368, verifies also the destination directory by mistake. Let's
partially rework the problematic commit to avoid this regression.

Fixes: https://gitlab.gnome.org/GNOME/gnome-autoar/-/issues/36

gnome-autoar/autoar-extractor.c
@@ -907,7 +907,8 @@ autoar_extractor_do_sanitize_pathname (AutoarExtractor *self,
     extracted_filename = g_file_get_child (destination, basename);
   }
 
-  if (self->prefix != NULL && self->new_prefix != NULL) {
+  if (self->prefix != NULL && self->new_prefix != NULL &&
+      !g_file_equal (self->prefix, self->new_prefix)) {
     g_autofree char *relative_path;
     /* Replace the old prefix with the new one */
     relative_path = g_file_get_relative_path (self->prefix,
@@ -1829,11 +1830,6 @@ autoar_extractor_step_decide_destination (AutoarExtractor *self)
                                                 self->prefix,
                                                 files,
                                                 &self->new_prefix);
-
-    if (self->new_prefix && g_file_equal (self->prefix, self->new_prefix)) {
-      /* This prevents redundant path name handling later. */
-      g_clear_object (&self->new_prefix);
-    }
   } else {
     autoar_extractor_signal_decide_destination (self,
                                                 self->destination_dir,